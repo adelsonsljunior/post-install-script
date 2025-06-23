fa6031dfbccf71b195c0708974637386fbb4458f
adelsonsljunior <junioradelsonst@gmail.com>
2025-06-23 10:41:12 -0300

    remove bruno

--- install.sh
+++ install.sh
@@ -19,7 +19,6 @@ APT_PROGRAMS=(
     code
     vagrant
     zsh
-    bruno
     tilix
 )
 
@@ -105,11 +104,6 @@ ADD_EXTERN_REPOS() {
     sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
     echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
     rm -f packages.microsoft.gpg
-
-    #BRUNO
-    echo -e "${GREEN}[INFO] - Adicionando repositório do Bruno.${DEFAULT}"
-    sudo gpg --no-default-keyring --keyring /etc/apt/keyrings/bruno.gpg --keyserver keyserver.ubuntu.com --recv-keys 9FA6017ECABE0266
-    echo "deb [signed-by=/etc/apt/keyrings/bruno.gpg] http://debian.usebruno.com/ bruno stable" | sudo tee /etc/apt/sources.list.d/bruno.list 
 }
 
 VSCODE_INSTALL_EXTENSIONS() {
